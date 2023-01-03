ddd7ce85dc3c5b9132ef71fdfba4a552b70fc707
VetaShine <111084369+VetaShine@users.noreply.github.com>
2023-01-03 15:42:05 +0300

    Update app.py

--- bot/app.py
+++ bot/app.py
@@ -1,18 +1,21 @@
+import os 
 import asyncio
 import logging
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from client import MyRpcClient
 from handler import HandlerMessages
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aioredis import Redis
+import redis
 
 logging.basicConfig(level = logging.INFO)
 
 async def prepare(sendler: MyRpcClient) -> Dispatcher:
     """Создание и конфигурация бота, подключение к RabbitMQ"""
-    storage = MemoryStorage()
-    bot = Bot(" ")
-    dp = Dispatcher(bot, storage=storage)
+    storage = RedisStorage2(host = os.environ['REDIS_HOST'], db = 5, port = os.environ['REDIS_PORT'], password = os.environ['REDIS_PASSWORD'])
+    bot = Bot(os.environ['TOKEN'])
+    dp = Dispatcher(bot, storage = storage)
     handler = HandlerMessages(dp, sendler)
     handler.register_all_handlers()
     return dp
