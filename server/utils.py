c95cc8854c3e14a23c5457fa5d7e161476ad17fc
VetaShine <111084369+VetaShine@users.noreply.github.com>
2023-01-05 13:13:08 +0300

    Update utils.py

--- server/utils.py
+++ server/utils.py
@@ -2,11 +2,13 @@ from torchvision import transforms
 import torch
 import numpy as np
 
+# Среднее значение и стандартное отклонение, используемые для предварительно обученных моделей PyTorch
 mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
 
 
 def gram_matrix(y):
+    """ Матрица Грама для вычисления потери стиля """
     (b, c, h, w) = y.size()
     features = y.view(b, c, w * h)
     features_t = features.transpose(1, 2)
@@ -15,6 +17,7 @@ def gram_matrix(y):
 
 
 def train_transform(image_size):
+    """ Преобразование изображений обучения """
     transform = transforms.Compose(
         [
             transforms.Resize(int(image_size * 1.15)),
@@ -26,19 +29,22 @@ def train_transform(image_size):
     return transform
 
 
-def style_transform(image_size=None):
+def style_transform(image_size = None):
+    """ Преобразование стилевых изображений """
     resize = [transforms.Resize(image_size)] if image_size else []
     transform = transforms.Compose(resize + [transforms.ToTensor(), transforms.Normalize(mean, std)])
     return transform
 
 
 def denormalize(tensors):
+    """ Денормализация тензоров изображений с использованием среднего и стандартного отклонения """
     for c in range(3):
         tensors[:, c].mul_(std[c]).add_(mean[c])
     return tensors
 
 
 def deprocess(image_tensor):
+    """ Денормализация и масштабирование тензора изображения """
     image_tensor = denormalize(image_tensor)[0]
     image_tensor *= 255
     image_np = torch.clamp(image_tensor, 0, 255).cpu().numpy().astype(np.uint8)
