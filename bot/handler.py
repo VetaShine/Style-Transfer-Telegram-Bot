6ca8334c9fa4833723a4a76a555c8a399f047f06
VetaShine <111084369+VetaShine@users.noreply.github.com>
2023-01-09 12:40:17 +0300

    Update handler.py

--- bot/handler.py
+++ bot/handler.py
@@ -5,9 +5,9 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters import Text
 from aiogram import types
-from client import MyRpcClient
+from client import MyClient
 
-class Gen(StatesGroup):
+class Generation(StatesGroup):
     wait_for_style = State()
     wait_for_answer = State()
 
@@ -15,12 +15,12 @@ class HandlerMessages:
     def __init__(
             self,
             dispatcher: Dispatcher,
-            sendler: MyRpcClient
+            sendler: MyClient
     ):
         self._dispatcher = dispatcher
         self._sendler = sendler
 
-    def register_start_message_handler(self) -> None:
+    def start_message_handler(self) -> None:
         """ Начало работы с ботом """
         @self._dispatcher.message_handler(commands = "start")
         async def starting_bot(message: types.Message):
@@ -37,10 +37,10 @@ class HandlerMessages:
             markup = types.ReplyKeyboardMarkup(resize_keyboard = True, row_width = 1)
             button = ["В.В.Кандинский «Композиция VII»", "К.Хокусай «Большая волна в Канагаве»", "И.К.Айвазовский «Океан»", "Акварельные краски", "Э.Р.Кальзадо «Начало»"]
             markup.add(*button)
-            await Gen.wait_for_style.set()
+            await Generation.wait_for_style.set()
             await message.answer("Отлично! У меня есть несколько стилей, которые я могу предложить тебе. Выбери заинтересовавший стиль.",  reply_markup = markup)
 
-    def input_message_and_wait_handler(self) -> None:
+    def consent_to_generate(self) -> None:
         """ Согласие пользователя на стилизацию """
         @self._dispatcher.message_handler(Text(equals = "Да, конечно!"))
         async def await_input_from_user(message: types.Message):
@@ -58,11 +58,11 @@ class HandlerMessages:
 
     def send_and_reply_message(self) -> None:
         """ Получение ответа от пользователя, отправка запроса к серверу, сброс состояния выбора стиля, перевод в состояния ожидания генерации """
-        @self._dispatcher.message_handler(state = Gen.wait_for_style)
+        @self._dispatcher.message_handler(state = Generation.wait_for_style)
         async def answer_on_input(message: types.Message, state: FSMContext):
             await message.answer("Нужно чуточку подождать!")
             await state.finish()
-            await Gen.wait_for_answer.set()
+            await Generation.wait_for_answer.set()
             # Запрос к серверу в зависимости от выбранного пользователем стиля 
             if(message.text == "В.В.Кандинский «Композиция VII»"):
                 await self.get_answer_and_reply(message.from_user.id, '/app/checkpoints/kandinskyVII_10000.pth', state)
@@ -75,22 +75,22 @@ class HandlerMessages:
             elif(message.text == "Э.Р.Кальзадо «Начало»"):
                 await self.get_answer_and_reply(message.from_user.id, '/app/checkpoints/kalzado_10000.pth', state)
     
-    def send_bot_about(self) -> None:
+    def refusal_to_generate(self) -> None:
         """ Отказ пользователя от стилизации """
         @self._dispatcher.message_handler(Text(equals = "Нет, не хочу."))
         async def not_bot(message: types.Message):
             await message.answer("Жаль! Отправь мне «/start», если всё-таки захочешь приступить к стилизации изображения.")   
-
+   
     def block_message_for_generation(self) -> None:
         """ Блокирование пользователю новых запросов, пока не будет получен ответ от сервера """
-        @self._dispatcher.message_handler(content_types=['text'], state = Gen.wait_for_answer)
+        @self._dispatcher.message_handler(content_types = ['text'], state = Generation.wait_for_answer)
         async def warning_gen(message: types.Message, state: FSMContext):
             await message.answer("Сначала необходимо дождаться окончания генерации!")
             
     def register_all_handlers(self) -> None:
-        self.register_start_message_handler()
+        self.start_message_handler()
         self.input_photo()
-        self.input_message_and_wait_handler()
+        self.consent_to_generate()
         self.send_and_reply_message()
-        self.send_bot_about()
+        self.refusal_to_generate()
         self.block_message_for_generation()
